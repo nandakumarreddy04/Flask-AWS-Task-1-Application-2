2499fdfee6c38b05967620fa5859694ea521a125
Nanda Kumar Reddy Gollapalli <115801299+nandakumarreddy04@users.noreply.github.com>
2024-01-31 11:15:05 +0530

    Update main2.py

--- main2.py
+++ main2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def helloworld():
-	return "Hello World, from Nanda-2  (Load Balancer)"
+	return "<h1>Hello World, from Nanda-2  (Load Balancer)</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
